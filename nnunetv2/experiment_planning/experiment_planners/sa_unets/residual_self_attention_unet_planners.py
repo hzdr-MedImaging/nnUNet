30e21cf3bd43b6fc2f3344ffe034a09bbce0d7f8
Pavel Nikulin <p.nikulin@hzdr.de>
2025-09-24 16:02:32 +0200

    Self-attention UNet

--- nnunetv2/experiment_planning/experiment_planners/sa_unets/residual_self_attention_unet_planners.py
+++ nnunetv2/experiment_planning/experiment_planners/sa_unets/residual_self_attention_unet_planners.py
@@ -14,7 +14,8 @@ from nnunetv2.experiment_planning.experiment_planners.residual_unets.residual_en
 
 from nnunetv2.experiment_planning.experiment_planners.network_topology import get_pool_and_conv_props
 
-
+# Does not really respect the memory limits. Just adds SA blocks to the ResEncUnet (which is configured with the
+# respective limits in mind).
 class ResEncUNetWithSAPlanner(ResEncUNetPlanner):
     def __init__(self, dataset_name_or_id: Union[str, int],
                  gpu_memory_target_in_gb: float = 8,
@@ -124,23 +125,3 @@ class nnUNetPlannerResEncSAXL(ResEncUNetWithSAPlanner):
         self.UNet_reference_val_3d = 3600000000
         self.UNet_reference_val_2d = 560000000
         self.max_dataset_covered = 1
-
-
-if __name__ == '__main__':
-    # we know both of these networks run with batch size 2 and 12 on ~8-10GB, respectively
-    net = ResidualEncoderUNet(input_channels=1, n_stages=6, features_per_stage=(32, 64, 128, 256, 320, 320),
-                              conv_op=nn.Conv3d, kernel_sizes=3, strides=(1, 2, 2, 2, 2, 2),
-                              n_blocks_per_stage=(1, 3, 4, 6, 6, 6), num_classes=3,
-                              n_conv_per_stage_decoder=(1, 1, 1, 1, 1),
-                              conv_bias=True, norm_op=nn.InstanceNorm3d, norm_op_kwargs={}, dropout_op=None,
-                              nonlin=nn.LeakyReLU, nonlin_kwargs={'inplace': True}, deep_supervision=True)
-    print(net.compute_conv_feature_map_size((128, 128, 128)))  # -> 558319104. The value you see above was finetuned
-    # from this one to match the regular nnunetplans more closely
-
-    net = ResidualEncoderUNet(input_channels=1, n_stages=7, features_per_stage=(32, 64, 128, 256, 512, 512, 512),
-                              conv_op=nn.Conv2d, kernel_sizes=3, strides=(1, 2, 2, 2, 2, 2, 2),
-                              n_blocks_per_stage=(1, 3, 4, 6, 6, 6, 6), num_classes=3,
-                              n_conv_per_stage_decoder=(1, 1, 1, 1, 1, 1),
-                              conv_bias=True, norm_op=nn.InstanceNorm2d, norm_op_kwargs={}, dropout_op=None,
-                              nonlin=nn.LeakyReLU, nonlin_kwargs={'inplace': True}, deep_supervision=True)
-    print(net.compute_conv_feature_map_size((512, 512)))  # -> 129793792
